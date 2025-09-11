b9f0c616c5042f7764e188a536b57a7a42e9713d
ferozeab <102701562+ferozeab@users.noreply.github.com>
2025-09-12 11:04:53 +1200

    Create Personal_greet.py

---
+++ Personal_greet.py
@@ -0,0 +1,7 @@
+#Python Create a Personalized Greeting
+b=input('Enter the first name:')
+c=input('Enter the last name:')
+b=str(b)
+c=str(c)
+
+print('Hello, ',b,c,'\b!','Welcome to the Python program.')
