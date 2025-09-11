6af29f088b0a1f391ecca208e734a71264965e68
ferozeab <102701562+ferozeab@users.noreply.github.com>
2025-09-12 10:57:57 +1200

    Create mathemetical.py

---
+++ mathemetical.py
@@ -0,0 +1,19 @@
+#Python Simple Mathemetical script
+b=input('Enter the first number:')
+c=input('Enter the second number:')
+b=int(b)
+c=int(c)
+a=b+c
+n=b-c
+m=b*c
+d=b/c
+
+a=int(a)
+n=int(n)
+m=int(m)
+d=float(d)
+
+print('Addition: ',a)
+print('Subtraction: ',n)
+print('Multiplication: ',m)
+print('Division: ',d)
