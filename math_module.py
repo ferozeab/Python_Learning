aa7be03c7326bdc9b5912b0e0802e7d5e5543c24
ferozeab <102701562+ferozeab@users.noreply.github.com>
2025-09-17 12:10:28 +1200

    Create math_module.py

---
+++ math_module.py
@@ -0,0 +1,11 @@
+#Using the Math Module for Calculations
+
+import math
+
+i=int(input('Enter a number:'))
+
+sq = float(math.sqrt(i))
+l = float(math.log(i))
+si = float(math.sin(i))
+
+print('Square root:',sq,'\nLogarithm:',l,'\nSine:',si)
